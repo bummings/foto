fe2879b50adc47e628253aebbcd5a9342358910c
bummings <eabummings@gmail.com>
2019-08-24 15:03:25 -0400

    cleaning up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
-import './App.css';
 import './App.scss';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hello: 'i forgot how to react'
+    };
+  }
+
   render() {
     return (
       <div className='app'>
-        <h1 className='app__header'>not u</h1>
+        <h1 className='app__header'>foto thing</h1>
+        <h2 className='app__text'>{this.state.hello}</h2>
       </div>
     );
   }
