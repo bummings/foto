6e9f3ac8225479ef999ea6c784008627474a51a3
bummings <eabummings@gmail.com>
2019-08-24 14:06:26 -0400

    git is killing me

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1 className='app__header'>not u</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
